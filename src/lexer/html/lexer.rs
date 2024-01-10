9977544500d5db7803689593fed0d3698f3d9469
ITTMT <oliver.iasmith@gmail.com>
2024-01-10 12:44:27 +0000

    Pushing work to laptop
    Going to prioritise the vs code extension work, the lexer isn't really that important to get done since it's mostly already been done hundreds of times before by other people.

--- src/lexer/html/lexer.rs
+++ src/lexer/html/lexer.rs
@@ -1,11 +1,134 @@
+use std::fmt::Display;
+
+// Following the whatwg spec
+// https://html.spec.whatwg.org/multipage/parsing.html#data-state
+
 pub enum Token {
-    Ampersand,  // (&) Switch to character reference in data state
-    LPointy,    // (<) Tag open state
-    Error,      // Parse Error
 
 
 }
 
-// I will re-consider making my own lexer, if the time comes for it. Because HTML can technically store both JS and CSS
-// in it which will be a lot of work. HTML5Tokenizer crate exists, and seems to do what is necessary,
-// delete this later if it comes to it.
+pub enum State {
+    Data,
+    RCData,
+    RawText,
+    Script,
+    PlainText,
+    TagOpen,
+    EndTagOpen,
+    TagName,
+    RCDataLessThan,
+    RCDataEndTagOpen,
+    RCDataEndTagName,
+    RawTextLessThan,
+    RawTextEndTagOpen,
+    RawTextEndTagName,
+    ScriptDataLessThan,
+    ScriptDataEndTagOpen,
+    ScriptDataEndTagName,
+    ScriptDataEscapeStart,
+    ScriptDataEscapeStartDash,
+    ScriptDataEscaped,
+    ScriptDataEscapedDash,
+    ScriptDataEscapedDashDash,
+    ScriptDataEscapedLessThan,
+    ScriptDataEscapedEndTagOpen,
+    ScriptDataDoubleEscapeStart,
+    ScriptDataDoubleEscaped,
+    ScriptDataDoubleEscapedDash,
+    ScriptDataDoubleEscapedDashDash,
+    ScriptDataDoubleEscapedLessThanSign,
+    ScriptDataDoubleEscapeEnd,
+    BeforeAttributeName,
+    AttributeName,
+    AfterAttributeName,
+    BeforeAttributeValue,
+    AttributeValueDoubleQuoted,
+    AttributeValueSingleQuoted,
+    AttributeValueUnquoted,
+    AfterAttributeValueQuoted,
+    SelfClosingStartTag,
+    BogusComment,
+    MarkupDeclarationOpen,
+    CommentStart,
+    CommentStartDash,
+    Comment,
+    CommentLessThan,
+    CommentLessThanBang,
+    CommentLessThanBangDash,
+    CommentLessThanBangDashDash,
+    CommentEndDash,
+    CommentEnd,
+    CommentEndBang,
+    DocType,
+    BeforeDocType,
+    DocTypeName,
+    AfterDocTypeName,
+    AfterDocTypePublicKeyword,
+    BeforeDocTypePublicIdentifier,
+    DocTypePublicIdentifierDoubleQuoted,
+    DocTypePublicIdentifierSingleQuoted,
+    AfterDocTypePublicIdentifier,
+    BetweenDocTypePublicAndSystemIdentifiers,
+    AfterDocTypeSystemKeyword,
+    BeforeDocTypeSystemIdentifier,
+    DocTypeSystemIdentifierDoubleQuoted,
+    DocTypeSystemIdentifierSingleQuoted,
+    AfterDocTypeSystemIdentifier,
+    BogusDocType,
+    CDataSection,
+    CDataSectionBracket,
+    CDataSectionEnd,
+    CharacterReference,
+    NamedCharacterReference,
+    AmbiguousAmpersand,
+    NumericCharacterReference,
+    HexadecimalCharacterReferenceStart,
+    DecimalCharacterReferenceStart,
+    HexadecimalCharacterReference,
+    DecimalCharacterReference,
+    NumericCharacterReferenceEnd,
+}
+
+
+/// What the format should look like if we do print!()
+// impl Display for Token {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         return match self {
+//
+//         }
+//     }
+// }
+
+pub struct Lexer {
+    position: usize,
+    read_position: usize,
+    ch: u8,
+    input: Vec<u8>,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Lexer {
+        let mut lex = Lexer {
+            position: 0,
+            read_position: 0,
+            ch: 0,
+            input: input.into_bytes(),
+        };
+
+        lex.read_char();
+
+        return lex;
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -44,3 +44,15 @@ fn tokenize_html(file_contents :&String) {
 // https://github.com/microsoft/vscode/tree/main/extensions/html-language-features/server/src
 // It's Microsoft's TypeScript implementation of HTML, it's a mess to try and follow.
 // Looking at what an ideal JSON layout might be for it. What information is required to be recorded.
+
+// https://www.w3.org/TR/2011/WD-html5-20110113/tokenization.html
+
+// it might work better if it was something like
+// open tag char detected -> begin open state -> accumulate chars until white-space / special / invalid / close tag
+// char -> parse
+
+
+// inline js and css might be a pain in the arse, especially if I want to add quick editing rules, like
+// move to file. I would maybe need to write a tokenizer for both of those as well to be accessed under the same LSP.
+// HTML5Tokenizer might not be enough since it is made to only work with HTML, the goal of the project is to have
+// better interoperability between html and css, so the LSP would be better if it was context-aware of the two.
